ea8198e9ff7b4b4285a5aef27ed1b3136236a1f8
AI_intelligence_genZ <natthanathron@gmail.com>
2026-02-09 13:44:44 +0700

    1.1.2

--- esp32.ino
+++ esp32.ino
@@ -1,7 +1,7 @@
 /*************** BLYNK ****************/
-#define BLYNK_TEMPLATE_ID   "TMPL6omEKuLcS"
+#define BLYNK_TEMPLATE_ID "TMPL6omEKuLcS"
 #define BLYNK_TEMPLATE_NAME "Quickstart Template"
-#define BLYNK_AUTH_TOKEN    "JPMDphcRk51qUVzCl95tWOasDLr5IEtz"
+#define BLYNK_AUTH_TOKEN "JPMDphcRk51qUVzCl95tWOasDLr5IEtz"
 
 #define BLYNK_PRINT Serial
 
@@ -16,65 +16,77 @@ char pass[] = "natthanathron";
 /*************** SERIAL ****************/
 #define RXD2 16
 #define TXD2 17
-HardwareSerial SerialESP(2);   // UART2
+HardwareSerial SerialESP(2); // UART2
 
 /*************** TIMER *****************/
 BlynkTimer timer;
 
 /*************** COMMAND ***************/
-int pumpCommand[2]       = {0, 0};
-int shinyCommand[4]      = {0, 0, 0, 0};
+int pumpCommand[2] = {0, 0};
+int shinyCommand[4] = {0, 0, 0, 0};
 
-int lastPumpCommand[2]   = {-1, -1};
-int lastShinyCommand[4]  = {-1, -1, -1, -1};
+int lastPumpCommand[2] = {-1, -1};
+int lastShinyCommand[4] = {-1, -1, -1, -1};
 
 /*************** PH ********************/
 float pH_Value = 0;
 
 /*************** SEND TO MEGA **********/
-void sendToMega(const char* tag, int *command, int len) {
+void sendToMega(const char *tag, int *command, int len)
+{
 
   Serial.print("[SEND] ");
   Serial.print(tag);
   Serial.print(" -> ");
-  for (int i = 0; i < len; i++) {
+  for (int i = 0; i < len; i++)
+  {
     Serial.print(command[i]);
-    if (i < len - 1) Serial.print(",");
+    if (i < len - 1)
+      Serial.print(",");
   }
   Serial.println();
 
   SerialESP.print("<");
   SerialESP.print(tag);
   SerialESP.print(":");
-  for (int i = 0; i < len; i++) {
+  for (int i = 0; i < len; i++)
+  {
     SerialESP.print(command[i]);
-    if (i < len - 1) SerialESP.print(",");
+    if (i < len - 1)
+      SerialESP.print(",");
   }
   SerialESP.println(">");
 }
 
 /*************** CHECK & SEND **********/
-void checkAndSend() {
+void checkAndSend()
+{
 
-  bool pumpChanged  = false;
+  bool pumpChanged = false;
   bool shinyChanged = false;
 
-  for (int i = 0; i < 2; i++) {
-    if (pumpCommand[i] != lastPumpCommand[i]) {
+  for (int i = 0; i < 2; i++)
+  {
+    if (pumpCommand[i] != lastPumpCommand[i])
+    {
       lastPumpCommand[i] = pumpCommand[i];
       pumpChanged = true;
     }
   }
 
-  for (int i = 0; i < 4; i++) {
-    if (shinyCommand[i] != lastShinyCommand[i]) {
+  for (int i = 0; i < 4; i++)
+  {
+    if (shinyCommand[i] != lastShinyCommand[i])
+    {
       lastShinyCommand[i] = shinyCommand[i];
       shinyChanged = true;
     }
   }
 
-  if (pumpChanged)  sendToMega("PUMP",  pumpCommand,  2);
-  if (shinyChanged) sendToMega("SHINY", shinyCommand, 4);
+  if (pumpChanged)
+    sendToMega("PUMP", pumpCommand, 2);
+  if (shinyChanged)
+    sendToMega("SHINY", shinyCommand, 4);
 }
 
 /*************** BLYNK INPUT ***********/
@@ -89,18 +101,23 @@ BLYNK_WRITE(V4) { shinyCommand[2] = param.asInt(); }
 BLYNK_WRITE(V5) { shinyCommand[3] = param.asInt(); }
 
 /*************** READ PH FROM MEGA *****/
-void readPHfromMega() {
+void readPHfromMega()
+{
   static String buffer = "";
 
-  while (SerialESP.available()) {
+  while (SerialESP.available())
+  {
     char c = SerialESP.read();
 
-    if (c == '<') {
+    if (c == '<')
+    {
       buffer = "<";
     }
-    else if (c == '>') {
+    else if (c == '>')
+    {
       buffer += ">";
-      if (buffer.startsWith("<PH:")) {
+      if (buffer.startsWith("<PH:"))
+      {
         pH_Value = buffer.substring(4, buffer.length() - 1).toFloat();
         Serial.print("[ESP] pH = ");
         Serial.println(pH_Value, 2);
@@ -108,35 +125,40 @@ void readPHfromMega() {
       }
       buffer = "";
     }
-    else {
+    else
+    {
       buffer += c;
-      if (buffer.length() > 30) buffer = "";
+      if (buffer.length() > 30)
+        buffer = "";
     }
   }
 }
 
 /*************** KEEP ALIVE ************/
-void sendAlways() {
-  sendToMega("PUMP",  pumpCommand,  2);
+void sendAlways()
+{
+  sendToMega("PUMP", pumpCommand, 2);
   sendToMega("SHINY", shinyCommand, 4);
 }
 
 /*************** SETUP *****************/
-void setup() {
+void setup()
+{
   Serial.begin(115200);
   SerialESP.begin(115200, SERIAL_8N1, RXD2, TXD2);
 
   Blynk.begin(auth, ssid, pass);
 
-  timer.setInterval(1000L,  checkAndSend);   // ส่งเมื่อเปลี่ยน
-  timer.setInterval(2000L, sendAlways);     // keep alive
-  timer.setInterval(4000L,  readPHfromMega); // อ่าน pH
+  timer.setInterval(1000L, checkAndSend);
+  timer.setInterval(2000L, sendAlways);
+  timer.setInterval(4000L, readPHfromMega);
 
   Serial.println("ESP32 Started");
 }
 
 /*************** LOOP ******************/
-void loop() {
+void loop()
+{
   Blynk.run();
   timer.run();
 }
